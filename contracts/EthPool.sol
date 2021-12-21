5138a1dcdd66949d3d2457bded08a6f9d326c8b5
Tony Olendo <tony.olendo@gmail.com>
2021-12-21 02:46:10 -0400

    Added tests

--- contracts/EthPool.sol
+++ contracts/EthPool.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: Unlicenses
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
@@ -13,8 +12,8 @@ contract EthPool is Ownable {
   using SafeMath for uint256;
   using EnumerableSet for EnumerableSet.UintSet;
 
-  /* @dev 
-    Struct that implements the OZ Enumerable set.
+  /** 
+    @dev Struct that implements the OZ Enumerable set.
     This ensures all create, read, update operations are O(1).
     Iterating through the array is O(n).
   */
@@ -22,10 +21,16 @@ contract EthPool is Ownable {
     EnumerableSet.UintSet weeklyDepositSet;
   }
 
+  uint16 public maxNoOfUniqueWeeks;
+  uint public currentWeek;
+  uint internal constant PRECISION_OFFSET = 10 ** 4;
+
   mapping(address => uint) public deposits;
   
-  /* @dev depositsPerWeek maps unique weekly deposits, 
-  it maps weeknumber -> address -> total deposits for that week */
+  /**
+    @dev depositsPerWeek maps unique weekly deposits, 
+    it maps weeknumber -> address -> total deposits for that week
+  */
   mapping(uint => mapping(address => uint)) public depositsPerWeek;
   
   mapping(uint => uint) public totalRewardsPerWeek;
@@ -33,42 +38,70 @@ contract EthPool is Ownable {
   mapping(address => bool) private _allowList;
   mapping(address => UserDeposits) uniqueWeeklyDeposits;
 
-  uint public currentWeek;
-  uint internal constant PRECISION_OFFSET = 10 ** 4;
-
   event DepositReceived(address indexed depositor, uint amount);
   event RewardsReceived(address indexed rewarder, uint amount);
   event FundsWithdrawn(address indexed rewarder, uint amount);
   event WeekAdvanced(uint newWeek);
+  event UniqueMaxUpdated(uint16 newMax);
 
-  constructor(){
+  constructor(uint16 _maxNoOfUniqueWeeks){
     _allowList[msg.sender] = true;
+    maxNoOfUniqueWeeks = _maxNoOfUniqueWeeks;
   }
 
-  /**
-  * @dev Allow only whitelisted address.
-  */
+  /// @dev Allow only whitelisted address.
   modifier onlyAllowList() {
     require(_allowList[msg.sender], "EP ONLY_ALLOWLISTED");
     _;
   }
-
-  function showUniqueWeeklyDeposits(address depositor) public view returns (uint[] memory arr){
+  
+  /// @notice Show a depositors Unique Weekly Deposits.
+  /// @dev    This operation is O(n) and can consume considerable gas
+  function showUniqueWeeklyDeposits(address depositor) external view returns (uint[] memory arr){
     UserDeposits storage uniqueDeposits = uniqueWeeklyDeposits[depositor];
     arr = uniqueDeposits.weeklyDepositSet.values();
   }
 
+  /// @notice Update Max No. of Unique Weeks
+  function updateUniqueMaxNoOfWeeks(uint16 newMax) external onlyOwner {
+    maxNoOfUniqueWeeks = newMax;
+  }
+
+  /// @notice Show the total Unique Weekly Deposits for a given address.
+  /// @dev    This operation is O(1)
+  function totalUniqueWeeklyDeposits(address depositor) external view returns (uint){
+    UserDeposits storage uniqueDeposits = uniqueWeeklyDeposits[depositor];
+    return uniqueDeposits.weeklyDepositSet.length();
+  }
+
+  function allowList(address _address, bool allow) external onlyOwner {
+    _allowList[_address] = allow;
+  }
+
+  function advanceWeek() external onlyAllowList {
+    _advanceWeek();
+  }
+
+  /**
+    @notice Deposit funds into the pool
+    @dev Due to gas constraints in iterating through the array in rewards,
+    we limit the max of number of unique deposits to a value set by the owner
+  */
   function deposit() external payable {
     require(msg.value > 0, "EP SEND_MORE_ETHER");
     
+    UserDeposits storage _uniqueWeeklyDeposits = uniqueWeeklyDeposits[msg.sender];
+    EnumerableSet.UintSet storage weeklyDeposit = _uniqueWeeklyDeposits.weeklyDepositSet;
+    require(weeklyDeposit.length() < maxNoOfUniqueWeeks, "EP:: WITHDRAW_FUNDS_BEFORE_DEPOSITING");
+
     uint depositsThisWeek = depositsPerWeek[currentWeek][msg.sender];
     uint totalDeposits = deposits[msg.sender];
     uint _totalDepositsThisWeek = totalDepositsPerWeek[currentWeek];
 
-    UserDeposits storage _uniqueWeeklyDeposits = uniqueWeeklyDeposits[msg.sender];
-    EnumerableSet.UintSet storage weeklyDeposit = _uniqueWeeklyDeposits.weeklyDepositSet;
+    // Update Enumerable Set
     weeklyDeposit.add(currentWeek);
 
+    // Other State Updates
     depositsPerWeek[currentWeek][msg.sender] = depositsThisWeek.add(msg.value);
     deposits[msg.sender] = totalDeposits.add(msg.value);
     totalDepositsPerWeek[currentWeek] = _totalDepositsThisWeek.add(msg.value);
@@ -76,31 +109,39 @@ contract EthPool is Ownable {
     emit DepositReceived(msg.sender, msg.value);
   }
 
+  /// @notice Deposits rewards into a pool. 
+  /// Optional paramter to move the period to a new week.
   function reward(bool moveWeek) external payable onlyAllowList {
     require(msg.value > 0, "EP:: SEND_MORE_REWARDS");
     totalRewardsPerWeek[currentWeek] += msg.value;
-    if(moveWeek){
+    
+    if(moveWeek) {
       _advanceWeek();
     }
+
     emit RewardsReceived(msg.sender, msg.value);
   }
 
-  function withdraw(address payable to) external payable returns (uint) {
+  function withdraw(address payable to) external payable {
     require(deposits[msg.sender] > 0, "EP:: NO_FUNDS_DEPOSITED");
     
     uint totalEligibleRewards;
     uint totalUserDeposits = deposits[msg.sender];
     deposits[msg.sender] = 0;
     
+    /// Initialize memory array based on Enumerable Set
     UserDeposits storage _uniqueWeeklyDeposits = uniqueWeeklyDeposits[msg.sender];
+    EnumerableSet.UintSet storage weeklyDeposit = _uniqueWeeklyDeposits.weeklyDepositSet;
     uint[] memory arr = _uniqueWeeklyDeposits.weeklyDepositSet.values();
 
+    /// Calculate Rewards by iterating through weeks
     for (uint i = 0; i < arr.length; i++) {
       uint _totalWeeklyRewards = totalRewardsPerWeek[arr[i]];
       uint _totalUserWeeklyDeposits = depositsPerWeek[arr[i]][msg.sender] * PRECISION_OFFSET;
       uint _totalDepositsPerWeek = totalDepositsPerWeek[arr[i]];
 
       depositsPerWeek[arr[i]][msg.sender] = 0;
+      weeklyDeposit.remove(i);
 
       if(_totalWeeklyRewards > 0){
         uint shareOfWeeklyDeposits = _totalUserWeeklyDeposits.div(_totalDepositsPerWeek);
@@ -111,16 +152,7 @@ contract EthPool is Ownable {
     uint withdrawAmount = totalEligibleRewards + totalUserDeposits;
     (bool sent,) = to.call{value: withdrawAmount}("");
     require(sent, "EP:: ETHER_NOT_SENT");
-    
     emit FundsWithdrawn(msg.sender, withdrawAmount);
-  }
-
-  function advanceWeek() external onlyAllowList {
-    _advanceWeek();
-  }
-
-  function allowList(address _address, bool allow) external onlyOwner {
-    _allowList[_address] = allow;
   }
 
   function _advanceWeek() internal {
--- test/ethpool.ts
+++ test/ethpool.ts
@@ -1,6 +1,6 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import { expect } from "chai"
-import { BigNumber } from "ethers"
+import { BigNumber, PayableOverrides } from "ethers"
 import { ethers } from "hardhat"
 import { EthPool, EthPool__factory } from "../typechain"
 
@@ -13,17 +13,22 @@ describe("EthPool", () => {
   let depositor3: SignerWithAddress
   const standardDeposit = 300
   const doubleDeposit = 600
+  const MAX_UNIQUE_WEEKLY_DEPOSITS = 5
 
   beforeEach( async () => {
     [owner, depositor1, depositor2, depositor3] = await ethers.getSigners()
     ethPoolContract = await ethers.getContractFactory("EthPool")
-    ethPool = await ethPoolContract.connect(owner).deploy()
+    ethPool = await ethPoolContract.connect(owner).deploy(MAX_UNIQUE_WEEKLY_DEPOSITS)
     await ethPool.deployed()
   })
 
   describe("Deployment", () => {
     it("current period is 0", async function () {
-      expect(await ethPool.currentPeriod()).to.equal(0)
+      expect(await ethPool.currentWeek()).to.equal(0)
+    })
+
+    it("current period is 0", async function () {
+      expect(await ethPool.currentWeek()).to.equal(0)
     })
   })
 
@@ -36,14 +41,14 @@ describe("EthPool", () => {
 
     it("updated deposits per week value correctly", async function () {
       const deposit = ethers.utils.parseEther(standardDeposit.toString())
-      const currentPeriod: BigNumber = await ethPool.currentPeriod()
+      const currentWeek: BigNumber = await ethPool.currentWeek()
       await ethPool.connect(depositor1).deposit({value: deposit})
-      expect(await ethPool.depositsPerWeek(currentPeriod, depositor1.address)).to.equal(deposit)
+      expect(await ethPool.depositsPerWeek(currentWeek, depositor1.address)).to.equal(deposit)
     })
 
     it("updates total deposits", async function () {
       const deposit = ethers.utils.parseEther(standardDeposit.toString())
-      const currentPeriod: BigNumber = await ethPool.currentPeriod()
+      const currentWeek: BigNumber = await ethPool.currentWeek()
       await ethPool.connect(depositor1).deposit({value: deposit})
       expect(await ethPool.deposits(depositor1.address)).to.equal(deposit)
     })
@@ -51,10 +56,10 @@ describe("EthPool", () => {
     it("updates total deposits for multiple deposits per week", async function () {
       const deposit = ethers.utils.parseEther(standardDeposit.toString())
       const doubleBalance = ethers.utils.parseEther(doubleDeposit.toString())
-      const currentPeriod: BigNumber = await ethPool.currentPeriod()
+      const currentWeek: BigNumber = await ethPool.currentWeek()
       await ethPool.connect(depositor1).deposit({value: deposit})
       await ethPool.connect(depositor1).deposit({value: deposit})
-      expect(await ethPool.depositsPerWeek(currentPeriod, depositor1.address)).to.equal(doubleBalance)
+      expect(await ethPool.depositsPerWeek(currentWeek, depositor1.address)).to.equal(doubleBalance)
     })
 
     it("updates total deposits for multiple deposits", async function () {
@@ -64,50 +69,117 @@ describe("EthPool", () => {
       await ethPool.connect(depositor1).deposit({value: deposit})
       expect(await ethPool.deposits(depositor1.address)).to.equal(doubleBalance)
     })
+
+    it("reverts if no of unique deposits exceeds max", async function () {
+      const deposit = ethers.utils.parseEther(standardDeposit.toString())
+      const reward = ethers.utils.parseEther(standardDeposit.toString())
+      for (let i = 0; i < MAX_UNIQUE_WEEKLY_DEPOSITS; i++) {
+        await ethPool.connect(depositor1).deposit({value: deposit})
+        await ethPool.connect(owner).reward(true, {value: deposit})
+      }
+      const trx = ethPool.connect(depositor1).deposit({value: deposit})
+      await expect(trx).to.be.revertedWith("EP:: WITHDRAW_FUNDS_BEFORE_DEPOSITING")
+    })
   })
 
   describe("Rewards", () => {
-    // it("only whitelist can upgrade rewards", async function () {
-    //   const reward = ethers.utils.parseEther(standardDeposit.toString())
-    //   const payload = {value: reward}
-    //   const trx = await ethPool.connect(depositor1).reward(false, payload)
-    //   expect(trx).to.be.revertedWith("EP ONLY_ALLOWLISTED")
-    // })
+    it("only whitelist can add rewards", async function () {
+      const reward = ethers.utils.parseEther(standardDeposit.toString())
+      const payload = {value: reward}
+      const trx = ethPool.connect(depositor1).reward(false, payload)
+      await expect(trx).to.be.revertedWith("EP ONLY_ALLOWLISTED")
+    })
 
     it("receives rewards from owner", async function () {
       const reward = ethers.utils.parseEther(standardDeposit.toString())
       const payload = {value: reward}
-      const currentPeriod = 0
+      const currentWeek = 0
       await ethPool.connect(owner).reward(false, payload)
-      const totalRewardsThisWeek = await ethPool.connect(owner).totalRewardsPerWeek(currentPeriod);
+      const totalRewardsThisWeek = await ethPool.connect(owner).totalRewardsPerWeek(currentWeek);
       expect(totalRewardsThisWeek).to.equal(reward)
     })
 
-    it("receives rewards an advances the week", async function () {
+    it("receives rewards and advances the week", async function () {
       const reward = ethers.utils.parseEther(standardDeposit.toString())
       const payload = {value: reward}
-      const expectedPeriod = 1
+      const expectedWeek = 1
       await ethPool.connect(owner).reward(true, payload)
-      const currentPeriod = await ethPool.connect(owner).currentPeriod()
-      expect(currentPeriod).to.equal(expectedPeriod)
+      const currentWeek = await ethPool.connect(owner).currentWeek()
+      expect(currentWeek).to.equal(expectedWeek)
     })
   })
 
   describe("Withdrawal", () => {
-    // it("reverts if no deposits have been received", async function () {
-    //   const trx = await ethPool.connect(depositor1).withdraw()
-    //   expect(trx).to.be.revertedWith("EP:: NO_FUNDS_DEPOSITED")
-    // })
+    it("reverts if no deposits have been received", async function () {
+      const trx = ethPool.connect(depositor1).withdraw(depositor1.address);
+      await expect(trx).to.be.revertedWith("EP:: NO_FUNDS_DEPOSITED")
+    })
 
-    it("withdraws direct deposits when no rewards submitted", async function () {
+    it("withdraws only direct deposits when no rewards submitted", async function () {
       const reward = ethers.utils.parseEther(standardDeposit.toString())
       const deposit = ethers.utils.parseEther(standardDeposit.toString())
       const payload = {value: reward}
-      const currentPeriod = 0
+      const currentWeek = 0
       await ethPool.connect(depositor1).deposit({value: deposit})
       await ethPool.connect(owner).reward(false, payload)
-      const totalRewardsThisWeek = await ethPool.connect(owner).totalRewardsPerWeek(currentPeriod);
+      const totalRewardsThisWeek = await ethPool.connect(owner).totalRewardsPerWeek(currentWeek);
       expect(totalRewardsThisWeek).to.equal(reward)
+    })
+
+    it("withdraws rewards ratio: 25%/75% of pool", async function () {
+      const reward = ethers.utils.parseEther((200).toString())
+      const deposit1 = ethers.utils.parseEther((100).toString())
+      const deposit2 = ethers.utils.parseEther((300).toString())
+      const depositor1TotalFunds = ethers.utils.parseEther((150).toString())
+
+      await ethPool.connect(depositor1).deposit({value: deposit1})
+      await ethPool.connect(depositor2).deposit({value: deposit2})
+      await ethPool.connect(owner).reward(false, {value: reward})
+      const trx = ethPool.connect(depositor1).withdraw(depositor1.address)
+      await expect(trx).to.emit(ethPool, "FundsWithdrawn").withArgs(depositor1.address, depositor1TotalFunds)
+    })
+
+    it("withdraws rewards ratio: 100% pool", async function () {
+      const reward = ethers.utils.parseEther((150).toString())
+      const deposit1 = ethers.utils.parseEther((100).toString())
+      const deposit2 = ethers.utils.parseEther((300).toString())
+      const depositor1TotalFunds = ethers.utils.parseEther((250).toString())
+
+      await ethPool.connect(depositor1).deposit({value: deposit1})
+      await ethPool.connect(owner).reward(true, {value: reward})
+      await ethPool.connect(depositor2).deposit({value: deposit2})
+      const trx = ethPool.connect(depositor1).withdraw(depositor1.address)
+      await expect(trx).to.emit(ethPool, "FundsWithdrawn").withArgs(depositor1.address, depositor1TotalFunds)
+    })
+
+    it("withdraws rewards ratio: 12.5%/37.5%/50% pool", async function () {
+      const reward = ethers.utils.parseEther((400).toString())
+      const deposit1 = ethers.utils.parseEther((100).toString())
+      const deposit2 = ethers.utils.parseEther((300).toString())
+      const deposit3 = ethers.utils.parseEther((400).toString())
+      const depositor2TotalFunds = ethers.utils.parseEther((450).toString())
+
+      await ethPool.connect(depositor1).deposit({value: deposit1})
+      await ethPool.connect(depositor2).deposit({value: deposit2})
+      await ethPool.connect(depositor3).deposit({value: deposit3})
+      await ethPool.connect(owner).reward(false, {value: reward})
+      const trx = ethPool.connect(depositor2).withdraw(depositor2.address)
+      await expect(trx).to.emit(ethPool, "FundsWithdrawn").withArgs(depositor2.address, depositor2TotalFunds)
+    })
+
+    it("withdraws low precision", async function () {
+      const reward = (587).toString()
+      const deposit1 = (123).toString()
+      const deposit2 = (237).toString()
+      const deposit3 = (122).toString()
+      const depositor2TotalFunds = (525).toString()
+
+      await ethPool.connect(depositor1).deposit({value: deposit1})
+      await ethPool.connect(depositor2).deposit({value: deposit2})
+      await ethPool.connect(depositor3).deposit({value: deposit3})
+      await ethPool.connect(owner).reward(false, {value: reward})
+      const trx = ethPool.connect(depositor2).withdraw(depositor2.address)
+      await expect(trx).to.emit(ethPool, "FundsWithdrawn").withArgs(depositor2.address, depositor2TotalFunds)
     })
   })
 })
